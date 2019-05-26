59e60b99c718b733080ef66846d756b11223bd81
Jared Parbs <japarbs@e1z2r3p42.42.us.org>
2019-05-26 15:42:37 -0700

    updated union

--- lv2/union/union.c
+++ lv2/union/union.c
@@ -13,12 +13,12 @@ int main(int ac, char **av)
 		return 0;
 	}
 	int i = -1;
-	char cpy[256];
+	int bindex;
 	unsigned char temp[128];
 	while (av[1][++i])
 	{
 		bindex = av[1][i];
-		if (temp[av[1][i]] == 0)
+		if (temp[bindex] == 0)
 			temp[bindex] = 1;
 	}
 	i = -1;
